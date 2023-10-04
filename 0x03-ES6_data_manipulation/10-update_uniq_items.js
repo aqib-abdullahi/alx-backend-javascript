6106deeeb1c945f3c0ee36c90f1fe273a0a4bb6f
aqib-abdullahi <abdullahiaaqib@gmail.com>
2023-10-04 17:02:42 +0000

    error handling for task 10(updating unique items in map) and weak map task

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('cannor process');
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+function queryAPI(endPoint) {
+  if (!weakMap.has(endPoint)) {
+    weakMap.set(endPoint, 1);
+  } else {
+    const counter = weakMap.get(endPoint) + 1;
+    weakMap.set(endPoint, counter);
+    if (counter >=5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
+
+export { queryAPI, weakMap };
