581a054068894118bb46a5bf2957a81bbedd1bbf
aqibabdullahi <abdullahiaqib@gmail.com>
2023-11-29 22:13:53 +0000

    handles EOF with stdout.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 process.stdin.setEncoding('utf-8');
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   const name = input.trim();
   if (name) {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
   } else {
-    console.log('Your name is: NULL');
+    process.stdout.write('Your name is: NULL');
   }
 
   process.stdin.on('close', () => {
-    console.log('This important software is now closing\n');
+    process.stdout.write('This important software is now closing\n');
   });
 });
