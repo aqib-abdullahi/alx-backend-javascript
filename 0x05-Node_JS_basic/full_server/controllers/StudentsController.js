344e01f9d22318d858107296666ae45aea43279e
aqibabdullahi <abdullahiaqib@gmail.com>
2023-11-30 18:59:46 +0000

    eslint fix

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,4 +1,4 @@
-const { readDatabase  } = require('../utils');
+const { readDatabase } = require('../utils');
 
 class StudentsController {
   static async getAllStudents(req, res) {
@@ -8,15 +8,15 @@ class StudentsController {
       let response = 'This is the list of our students';
       const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
       fields.forEach((field) => {
-          const students = data[field].join(', ');
-          response += `\nNumber of students in ${field}: ${data[field].length}. List: ${students}`;
+        const students = data[field].join(', ');
+        response += `\nNumber of students in ${field}: ${data[field].length}. List: ${students}`;
       });
       res.status(200).send(response);
     } catch (error) {
       res.status(500).send('Cannot load the database');
     }
   }
-  
+
   static async getAllStudentsByMajor(req, res) {
     const { major } = req.params;
     const allowedMajors = ['CS', 'SWE'];
@@ -25,7 +25,7 @@ class StudentsController {
       res.status(500).send('Major parameter must be CS or SWE');
       return;
     }
-    
+
     try {
       const db = './database.csv';
       const data = await readDatabase(db);
