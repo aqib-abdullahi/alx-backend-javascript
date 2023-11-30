b53e7d86394b0706738f16ec6b94c1e2f1a0aa1d
aqibabdullahi <abdullahiaqib@gmail.com>
2023-11-30 10:24:09 +0000

    added module.exports

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
+const hostname = 'localhost';
+const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, '127.0.0.1', () => {
-  console.log('Server running at http://127.0.0.1:1245/');
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
