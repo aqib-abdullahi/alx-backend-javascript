905c75bee92e51438a7945bea84394ed8accd854
aqibabdullahi <abdullahiaqib@gmail.com>
2023-12-02 10:30:53 +0000

    using sinon spy to test simple ADDITION function

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+    it('should call Utils.calculateNumber with correct arguments', function () {
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
+
+        calculateNumberSpy.restore();
+    });
+});
